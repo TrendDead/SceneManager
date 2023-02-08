99d4ec98ecd9d87ac37c890826f08804d058d29f
git - пше <89956045+TrendDead@users.noreply.github.com>
2023-02-08 17:47:35 +0700

    Добавление менеджера сцен

--- Assets/SceneManager/Scripts/Editor/SceneManager.cs
+++ Assets/SceneManager/Scripts/Editor/SceneManager.cs
@@ -4,42 +4,76 @@ using System.Linq;
 using System.IO;
 using UnityEditor.SceneManagement;
 
-/// <summary>
-/// Окно диспетчера сцен
-/// </summary>
-public class SceneManager : EditorWindow
+namespace UnityDev
 {
-    [MenuItem("UnityDev/Scene Manager/Creating scene manager window")]
-    public static void CreatingWindow()
+    /// <summary>
+    /// Окно диспетчера сцен
+    /// </summary>
+    public class SceneManager : EditorWindow
     {
-        EditorWindow.GetWindow<SceneManager>("Scene Manager");
-    }
-
-    private void OnGUI()
-    {
-        string[] scenePaths = EditorBuildSettings.scenes
-                .Where(scene => scene.enabled)
-                .Select(scene => scene.path)
-                .ToArray();
-        
-        var scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
-
-        for (int i = 0; i < scenePaths.Length; i++)
+        [MenuItem("UnityDev/Scene Manager/Creating scene manager window")]
+        public static void CreatingWindow()
         {
+            EditorWindow.GetWindow<SceneManager>("Scene Manager");
+        }
 
-            if (scene == EditorSceneManager.GetSceneByPath(scenePaths[i]))
-            {
-                GUI.backgroundColor = Color.green;
-            }
-            else
-            {
-                GUI.backgroundColor = Color.white;
-            }
+        private void OnGUI()
+        {
+            string[] scenePaths = EditorBuildSettings.scenes
+                    .Where(scene => scene.enabled)
+                    .Select(scene => scene.path)
+                    .ToArray();
 
-            if (GUILayout.Button(Path.GetFileNameWithoutExtension(scenePaths[i])))
+            var scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+        
+            for (int i = 0; i < scenePaths.Length; i++)
             {
-                EditorSceneManager.SaveScene(scene);
-                EditorSceneManager.OpenScene(scenePaths[i], OpenSceneMode.Single);
+
+                GUILayout.BeginHorizontal();
+
+                if (scene == EditorSceneManager.GetSceneByPath(scenePaths[i]))
+                {
+                    GUI.backgroundColor = Color.green;
+                }
+                else
+                {
+                    GUI.backgroundColor = Color.white;
+                }
+
+                //Кнопка выбора отдельной сцены
+                if (GUILayout.Button(Path.GetFileNameWithoutExtension(scenePaths[i])))
+                {
+                    foreach (var path in scenePaths)
+                    {
+                        EditorSceneManager.SaveScene(EditorSceneManager.GetSceneByPath(path));
+                    }
+                    EditorSceneManager.OpenScene(scenePaths[i], OpenSceneMode.Single);
+                }
+
+                if (EditorSceneManager.GetSceneByPath(scenePaths[i]).name != null)
+                {
+                    GUI.backgroundColor = Color.yellow;
+                }
+                else
+                {
+                    GUI.backgroundColor = Color.white;
+                }
+
+                //Кнопка совместного открытия сцен
+                if (GUILayout.Button("Additive"))
+                {
+                    if (EditorSceneManager.GetSceneByPath(scenePaths[i]).name == null)
+                    {
+                        EditorSceneManager.OpenScene(scenePaths[i], OpenSceneMode.Additive);
+                    }
+                    else
+                    {
+                        EditorSceneManager.SaveScene(EditorSceneManager.GetSceneByPath(scenePaths[i]));
+                        EditorSceneManager.UnloadSceneAsync(EditorSceneManager.GetSceneByPath(scenePaths[i]));
+                    }
+                }
+
+                GUILayout.EndHorizontal();
             }
         }
     }
